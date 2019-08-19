a414f86259a95d6c1db379814f1cc03d5f146de5
GORDON YANG (ITA-ISDC-ISD-OOCLL/ZHA) <YANGGO@OOCL.COM>
2019-08-19 22:25:55 +0800

    [refactor: change the updateQuality into Item and use the switch to discriminate]

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -12,70 +12,10 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    updateBackstagePasses(items[i]);
-                    continue;
-                }
-                else{
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        continue;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+            items[i].updateQuality();
         }
     }
 
-    public void updateBackstagePasses(Item item){
-        if (item.getQuality() < 50) {
-            item.setQuality(item.getQuality() + 1);
 
-            if (item.getSellIn() < 11) {
-                if (item.getQuality() < 50) {
-                    item.setQuality(item.getQuality() + 1);
-                }
-            }
 
-            if (item.getSellIn() < 6) {
-                if (item.getQuality() < 50) {
-                    item.setQuality(item.getQuality() + 1);
-                }
-            }
-        }
-
-        item.setSellIn(item.getSellIn() - 1);
-
-        if (item.getSellIn() < 0) {
-            item.setQuality(0);
-        }
-    }
 }
--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -52,4 +52,50 @@ public class Item {
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+
+    public void updateQuality() {
+
+        switch (this.name){
+            case "Aged Brie":
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+                sellIn = sellIn - 1;
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                if (quality < 50) {
+                    quality=quality + 1;
+                    if (sellIn < 11 && quality < 50) {
+                        quality = quality + 1;
+                    }
+                    if (sellIn < 6 && quality < 50 ) {
+                        quality = quality + 1;
+                    }
+                }
+                sellIn -= 1;
+                if (sellIn < 0) {
+                    quality = 0;
+                }
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+                break;
+            default:
+                if(quality > 0) quality = quality - 1;
+                sellIn = sellIn - 1;
+                if (sellIn < 0 && quality > 0) {
+                    quality = quality - 1;
+                }
+
+        }
+
+
+
+    }
+
+    public void updateBackstagePasses(){
+
+    }
 }
